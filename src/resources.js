0c2b787872fd2bb50c1ec9b58e5c3fb21d00386e
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-02-15 12:30:46 +0530

    Resources updated

--- src/resources.js
+++ src/resources.js
@@ -18,7 +18,42 @@ const resources = [
         name: "Web Site Design Inspiration",
         description : "A collection of beautiful webistes to take inspiration from",
         url : "https://www.webdesign-inspiration.com/"
-    }
+    },
+    {
+        name: "removebg",
+        description : "Remove Background from Image",
+        url : "https://www.remove.bg/"
+    },
+    {
+        name: "Tiny helpers",
+        description : "A collection of free single-purpose online tools for web developers...",
+        url : "https://tiny-helpers.dev/"
+    },
+    {
+        name: "Learn React",
+        description : "A Comprehensive React Course for beginners",
+        url : "https://scrimba.com/g/glearnreact"
+    },
+    {
+        name: "Resume Builder",
+        description : "A free online Resume Builder",
+        url : "https://enhancv.com/"
+    },
+    {
+        name: "Class Central",
+        description : "A search engine for courses",
+        url : "https://www.classcentral.com/"
+    },
+    {
+        name: "Command Line Essentials",
+        description : "Command Line Crash Course",
+        url : "https://learnpythonthehardway.org/book/appendixa.html"
+    },
+    {
+        name: "Git Command Explorer",
+        description : "Find the right commands you need without digging through the web.",
+        url : "https://gitexplorer.com/"
+    },
 ]
 
 export default resources;
